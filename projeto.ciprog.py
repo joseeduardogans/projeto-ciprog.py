08d3cf438019a6cf390148da11646bd9c5ab6a1e
joseeduardogans <josephgansoy@gmail.com>
2026-05-12 16:17:35 -0300

    Add files via upload

---
+++ projeto.ciprog.py
@@ -0,0 +1,7 @@
+print ("{}[=============================]{}\n | O SIGNIFICADO DA PAZ    	   |\n{}[=============================]{}\n")
+print ("Num ambiente universitário, donde movimenta-se muitas paixões e opinões entrelaçadas, de identidades muito diferenciadas e conflituosas entre si, você é um professor de matemática e aprece ter que lidar com algo não referente aos números. Dentro de um contexto geopolítico na qual ocorre guerras entre o Oriente e o Ocidente, um aluno faz a seguinte indação a ti:\n")
+print("Será que os Estados Unidos irá trazer a paz ao mundo lutando contra o Oriente?")
+print("escolha alguma das opções\n a) Provavelmente, pois a paz significa a conquista pela homogeanidade\n b) Não, pois o lado mais minoritario é essencialmente aquele que trará a paz\n c) Eu simplesmente não creio\n d) Talvez outros países trarão a paz")
+input()
+while not ("a"):
+    print("tente novamente")
