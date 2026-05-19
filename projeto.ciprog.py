73fb45297a27173faed499ba948fb7e9409a1cc8
joseeduardogans <josephgansoy@gmail.com>
2026-05-19 16:33:25 -0300

    Update projeto.ciprog.py

--- projeto.ciprog.py
+++ projeto.ciprog.py
@@ -1,7 +1,26 @@
-print ("{}[=============================]{}\n | O SIGNIFICADO DA PAZ    	   |\n{}[=============================]{}\n")
-print ("Num ambiente universitário, donde movimenta-se muitas paixões e opinões entrelaçadas, de identidades muito diferenciadas e conflituosas entre si, você é um professor de matemática e aprece ter que lidar com algo não referente aos números. Dentro de um contexto geopolítico na qual ocorre guerras entre o Oriente e o Ocidente, um aluno faz a seguinte indação a ti:\n")
-print("Será que os Estados Unidos irá trazer a paz ao mundo lutando contra o Oriente?")
-print("escolha alguma das opções\n a) Provavelmente, pois a paz significa a conquista pela homogeanidade\n b) Não, pois o lado mais minoritario é essencialmente aquele que trará a paz\n c) Eu simplesmente não creio\n d) Talvez outros países trarão a paz")
-input()
-while not ("a"):
-    print("tente novamente")
+c = []
+
+print("Fase 1")
+
+print ("Num ambiente universitário, donde movimenta-se muitas paixões e opinões entrelaçadas, de identidades muito diferenciadas e conflituosas entre si, você é um professor de matemática e aprece ter que lidar com algo não referente aos números. Dentro de um contexto geopolítico na qual ocorre guerras entre o Oriente e o Ocidente, um aluno faz a seguinte indação a ti\n\nSerá que o Ocidente irá trazer a paz ao mundo lutando contra o Oriente?\n\nEscolha alguma das opções\na) Provavelmente, pois a paz significa a conquista pela homogeanidade\nb) Não, pois o lado mais minoritario é essencialmente aquele que trará a paz\nc) Eu simplesmente não creio\nd) Talvez outros países trarão a paz\n")
+escolha = input("Escolha a opção\n")
+if escolha == 'c':
+    print('\nconseguiu\n')
+    
+elif escolha == 'd':
+        print('\nainda incerto, mas saiu do conflito\n')
+else:
+        print("\nfoi muito mal, 0 pontos\n")
+
+print ("Fase 2")
+
+print ("Chegando no campus para apresentar mais aulas, você vê 2 grupos de alunos de ideologias conflituosas provocando, mentalmente e fisicamente machucando entre si, o que você faz?\n\na) Toma um lado para entrar no conflitoz\nb) Simplesmte não faz nada e segue o eu caminho\nc) Observar e falar em voz alta o quanto aquilo é absurdo\nd) Chegar próximo de alguns alunos seus para explicar o que é correto\n")
+escolha = input("Escolha a opção\n")
+if escolha == 'd':
+    print('\nconseguiu\n')
+elif escolha == 'c':
+        print ('\nfoi bem\n')
+else:
+        print ('\n foi muito mal, 0 pontos\n')
+
+        
